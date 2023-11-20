ecc0096c614370d8a34cf1cb0dea88d37d996bd4
Sebstion <sebstachowski@gmail.com>
2023-11-20 08:13:02 -0500

    poprawka

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from calculator.views import hello
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello),
+    path('', include("calculator.urls")),
 ]
